e3a5bb76048b47f87b18c70c72e6456cfce5c724
Edmond Soun <edmond.soun@gmail.com>
2022-08-02 17:30:24 -0700

    table schema for bikes.sql

--- bikes.sql
+++ bikes.sql
@@ -3,3 +3,26 @@ CREATE TABLE customers (
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL
 );
+
+CREATE TABLE bikes (
+    code TEXT PRIMARY KEY,
+    model TEXT NOT NULL,
+    cost NUMERIC(8,2) NOT NULL
+);
+
+-- can we sum the sale_prices in order?
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    total NUMERIC(8,2) NOT NULL,
+    date_sold TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    customer_id INT NOT NULL REFERENCES customers
+);
+
+CREATE TABLE sold_bike (
+    id SERIAL PRIMARY KEY,
+    sale_price NUMERIC(8,2) NOT NULL DEFAULT REFERENCES bikes.cost, -- bikes.cost?
+    note TEXT DEFAULT "None",
+    bike_code TEXT NOT NULL REFERENCES bikes,
+    order_id INT NOT NULL REFERENCES orders
+);
+
